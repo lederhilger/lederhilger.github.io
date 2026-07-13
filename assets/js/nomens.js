cb422908179d18f506175d33e1589c95170596ed
simonled <simon.lederhilger@gmail.com>
2026-07-13 12:29:33 +0200

    Korean name order; Gothic last name changed on historical analysis.

--- assets/js/nomens.js
+++ assets/js/nomens.js
@@ -1,9 +1,9 @@
 const nomens = [
     "Σιμων Ανδρειδης",
-    "𐍃𐌴𐌹𐌼𐍉𐌽 𐍆𐌹𐌻𐌻𐌴𐌹𐌽𐌰𐌷𐌹𐌻𐌳𐌹𐌲𐌰𐌹𐍃",
+    "𐍃𐌴𐌹𐌼𐍉𐌽 𐍆𐌹𐌻𐌻𐌰𐌷𐌿𐌻𐍅𐌾𐌰",
     "莱德希尔格西蒙",
     "Симонъ Андрѣевичъ",
-    "시몬 레더힐거"
+    "레더힐거 시몬"
 ];
 const nomen = nomens[Math.floor(Math.random()*nomens.length)];
 document.getElementById("site-title").textContent = nomen;
